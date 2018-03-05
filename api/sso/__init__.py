899daedb473549df0e9a2eb867ac2d66ba50016c
Gibran Wisesa <gibranmfw@gmail.com>
2018-03-05 22:20:23 +0700

    add sso service skeleton/stubs

---
+++ api/sso/__init__.py
@@ -0,0 +1,22 @@
+class SSO:
+	""" 
+	This class is responsible for communicating with SSOAuth client
+	Any SSO related service should be implemented here.
+	"""
+
+	def __init__(user):
+		pass
+
+	def generate_token(self):
+		""" Generate javascript message to be send to SSOAuth client """
+		pass
+
+	def send_token(self):
+		""" Send message to SSOAuth service """
+		pass
+
+
+
+
+
+
