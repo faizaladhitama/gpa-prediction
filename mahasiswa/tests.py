c759aedb795770866d494934ee426968a7e844b1
faizaladhitama <faizaladhitamaprabowo@gmail.com>
2018-03-07 08:27:52 +0700

    [RED] client unittestaccess mahasiswa url

--- mahasiswa/tests.py
+++ mahasiswa/tests.py
@@ -1,2 +1,14 @@
 from django.test import TestCase
-# Create your tests here.
+
+class MahasiswaTest(TestCase):
+    def test_homepage(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_rekomendasi(self):
+        response = self.client.get('/rekomendasi-matkul')
+        self.assertEqual(response.status_code, 200)
+
+    def test_profile(self):
+        response = self.client.get('/profile')
+        self.assertEqual(response.status_code, 200)
