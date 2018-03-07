17b30533136a764553e4f88b76f92bdf10dd89e8
faizaladhitama <faizaladhitamaprabowo@gmail.com>
2018-03-07 10:00:38 +0700

    [RED] change test url on dosen/tests.py

--- dosen/tests.py
+++ dosen/tests.py
@@ -2,13 +2,13 @@ from django.test import TestCase
 
 class URLTest(TestCase):
     def test_homepage(self):
-        response = self.client.get('/')
+        response = self.client.get('/dosen',follow=True)
         self.assertEqual(response.status_code, 200)
 
     def test_carimahasiswa(self):
-        response = self.client.get('/cari-mahasiswa')
+        response = self.client.get('/dosen/cari-mahasiswa',follow=True)
         self.assertEqual(response.status_code, 200)
 
     def test_profile(self):
-        response = self.client.get('/profile')
+        response = self.client.get('/dosen/profile',follow=True)
         self.assertEqual(response.status_code, 200)
