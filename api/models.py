1b99a22a03051886a52c1b4a4d19ca052e0903cb
muhammadfaiz12 <faiz00.muhammad@gmail.com>
2018-03-07 21:55:31 +0700

    adding api in INSTALLED APPS on backend/setting.py

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Mahasiswa(models.Model):
+
+	npm = models.IntegerField()
+	angkatan = models.IntegerField()
+	nama = models.CharField(max_length = 20)
+
+class Matakuliah(models.Model):
+
+	kode_matkul = models.IntegerField()
+	nip_pengajar = models.IntegerField()
