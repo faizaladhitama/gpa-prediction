d4909be5558f285479586da56a6eecfff13efef9
muhammadfaiz12 <faiz00.muhammad@gmail.com>
2018-03-07 22:58:16 +0700

    makemigrations for db implemnentation , still have not done primary key implementation

--- api/models.py
+++ api/models.py
@@ -3,11 +3,43 @@ from django.db import models
 # Create your models here.
 class Mahasiswa(models.Model):
 
-	npm = models.IntegerField()
+	npm = models.IntegerField() #key
 	angkatan = models.IntegerField()
-	nama = models.CharField(max_length = 20)
+	prodi = models.CharField(max_length = 20)
+	nama = models.CharField(max_length = 100)
+	status_evaluasi = models.BooleanField()
 
-class Matakuliah(models.Model):
+class MataKuliah(models.Model):
 
-	kode_matkul = models.IntegerField()
+	kode_matkul = models.IntegerField() # key
 	nip_pengajar = models.IntegerField()
+	nama_matkul = models.CharField(max_length = 40)
+	prodi = models.CharField(max_length = 30)
+
+class Dosen(models.Model):
+	nip_pengajar = models.IntegerField()
+	nama_dosen = models.CharField(max_length = 100)
+	is_pa = models.BooleanField()
+
+class AnggotaKelas(models.Model):
+	npm = models.IntegerField()
+	kode_matkul = models.IntegerField()
+
+class PrasyaratMataKuliah:
+    kode_matkul = models.ForeignKey('MataKuliah', on_delete=models.CASCADE)
+    npm = models.ForeignKey('Mahasiswa', on_delete=models.CASCADE)
+
+class InformasiAkademis:
+	npm = models.ForeignKey('Mahasiswa', on_delete=models.CASCADE)
+	ip = models.IntegerField()
+	sks_dipunya = models.IntegerField()
+
+class PrediksiMataKuliah:
+	npm = models.ForeignKey('Mahasiswa', on_delete=models.CASCADE)
+	kode_matkul = models.ForeignKey('MataKuliah', on_delete=models.CASCADE)
+
+class RekamJejakNilaiMataKuliah:
+	npm = models.ForeignKey('Mahasiswa', on_delete=models.CASCADE)
+	kode_matkul = models.ForeignKey('MataKuliah', on_delete=models.CASCADE)
+	nilai = models.IntegerField()
+	Term = models.IntegerField()
