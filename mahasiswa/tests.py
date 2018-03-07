2e83b8e696c0bfe56109ab7692b2acbef4fab889
faizaladhitama <faizaladhitamaprabowo@gmail.com>
2018-03-07 08:35:29 +0700

    [RED] Change class name
    Change name to URLTest. This class focus on accessed url by client.

--- mahasiswa/tests.py
+++ mahasiswa/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-class MahasiswaTest(TestCase):
+class URLTest(TestCase):
     def test_homepage(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
