6ad4e0dab73edc72326d9dfea0c9f5e2544e4050
faizaladhitama <faizaladhitamaprabowo@gmail.com>
2018-03-08 22:56:41 +0700

    [RED] Add login and logout url test

--- api/tests.py
+++ api/tests.py
@@ -1,2 +1,13 @@
 from django.test import TestCase
-# Create your tests here.
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.firefox.webdriver import WebDriver
+
+class URLTest(TestCase):
+	def test_login(self):
+		response = self.client.get('login',follow=True)
+		self.assertEqual(response.status_code, 200)
+
+	def test_logout(self):
+		response = self.client.get('logout',follow=True)
+		self.assertEqual(response.status_code, 200)
+
