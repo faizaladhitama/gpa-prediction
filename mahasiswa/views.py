1269f8c6976d21e067815a9a042a52422a2b5021
sarah df <sarahdfxo@gmail.com>
2018-03-13 00:28:57 +0700

    add api views

--- mahasiswa/views.py
+++ mahasiswa/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
-	context = {'name':'mahasiswa'}
-	return render(request, 'mahasiswa/navbarMahasiswa.tpl', context)
+    context = {'name': 'mahasiswa'}
+    return render(request, 'mahasiswa/navbarMahasiswa.tpl', context)
+
 
 def profile(request):
-	context = {'name':'mahasiswa'}
-	return render(request, 'mahasiswa/profile.tpl', context)
+    context = {'name': 'mahasiswa'}
+    return render(request, 'mahasiswa/profile.tpl', context)
+
 
 def rekomendasi(request):
-	context = {'name':'mahasiswa'}
-	return render(request, 'mahasiswa/rekomendasi.tpl', context)
+    context = {'name': 'mahasiswa'}
+    return render(request, 'mahasiswa/rekomendasi.tpl', context)
