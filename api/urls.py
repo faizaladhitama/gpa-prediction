feb38f25b5c652be89738716dd80b4983d6f20e5
sarah df <sarahdfxo@gmail.com>
2018-03-13 00:33:09 +0700

    fix merge conflict

--- api/urls.py
+++ api/urls.py
@@ -22,15 +22,12 @@ from . import views
 CAS_LOGIN = django_cas_ng.views.login
 CAS_LOGOUT = django_cas_ng.views.logout
 urlpatterns = [
-<<<<<<< HEAD
     url(r'll^$', views.login, name='index'),
     url(r'^$', views.landing_page, name='landing_page'),
 	re_path(r'^login$', django_cas_ng.views.login,{"next_page":"","required":False},name='cas_ng_login'),
 	re_path(r'^logout$', django_cas_ng.views.logout,{"next_page":""}, name='cas_ng_logout')
-] 	
-=======
+	
     url(r'^$', views.login, name='index'),
     re_path(r'^login$', CAS_LOGIN, {"next_page": ""}, name='cas_ng_login'),
     re_path(r'^logout$', CAS_LOGOUT, {"next_page": ""}, name='cas_ng_logout')
 ]
->>>>>>> e408989957e0e2ddb0237887c5a798e45513cd52
