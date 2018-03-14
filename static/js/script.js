a72fc9d51ac13e4f04bf7a6565d1f396e97170ca
faizaladhitama <faizaladhitamaprabowo@gmail.com>
2018-03-14 11:09:51 +0700

    Change redirect url

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
 	$("#login-button").click(function() {
-	      window.location.href='https://sso.ui.ac.id/cas/login';
+	      window.location.href='/login';
 	});
 });
