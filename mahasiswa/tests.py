b455e78d182f18524990e4e74de5fc61ccc6d8b9
faizal.adhitama <faizal.adhitama@ui.ac.id>
2018-03-16 22:49:14 +0700

    [RED] Check html element on homepage

--- mahasiswa/tests.py
+++ mahasiswa/tests.py
@@ -14,7 +14,8 @@ class URLTest(TestCase):
         response = self.client.get('/mahasiswa/profile', follow=True)
         self.assertEqual(response.status_code, 200)
 
+
 class ElementTest(TestCase):
-	resp = self.client.get('/mahasiswa')
-     	with self.assertHTML(resp) as html:
-    		self.assertNotEqual(html.find('body/h10').text, '')
+    def test_homepage(self):
+        resp = self.client.get('/mahasiswa')
+        self.assertNotContains(response=resp, text="<h10>", status_code=301)
