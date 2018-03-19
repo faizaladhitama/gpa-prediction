babb2352035469f60e2b54589f3c2c9136c163bb
muhammadfaiz12 <faiz00.muhammad@gmail.com>
2018-03-20 00:28:08 +0700

    adding test unit and implementation for api/utils

--- api/db/test.py
+++ api/db/test.py
@@ -3,10 +3,47 @@ from requests.models import Response
 from django.test import TestCase
 from api.siak import get_academic_record, get_access_token
 from api.siak.utils import AuthGenerator, Requester
-from api.db.utils import getSiakData, credentialGenerator
+from api.db.utils import *
 
 class UtilsTest(TestCase):
-	def test_credentialGenerator(self):
-		assert(credentialGenerator() == AuthGenerator.get_access_token())
-	def test_getSiakData(self):
-		assert(getSiakData() != null)
+    
+    def setUp(self):
+        mocked_get_siak_data = patch('api.db.getSiakData') 
+        mocked_parse_siak_data = patch('api.db.parseSiakData')
+
+        self.mocked_get_siak_data.start()
+        self.mocked_parse_siak_data.start()
+
+        self.addCleanup(mocked_get_siak_data.stop)
+        self.addCleanup(mocked_parse_siak_data.stop)
+
+    def test_getSiakData(self):
+        self.mocked_generator.return_value = None
+        self.mocked_verify.return_value = {"username":"sonoko.nogi"}
+        self.mocked_requester.return_value = "mocked"
+        self.mocked_get_id.return_value = 1
+        self.mocked_get_token.return_value = 1
+        mock_npm = "mocked"
+        mock_username = "sonoko.nogi"
+        mock_password = "12345"
+
+        resp = getSiakData(mock_npm, mock_username, mock_password)
+
+        self.assertEqual("mocked", resp)
+    
+    def test_ParseSiak(self):
+        mock_npm = "mocked"
+        mock_username = "sonoko.nogi"
+        mock_password = "12345"
+        hasil = parseSiakDataToDb(mock_npm, mock_username, mock_password)
+        self.assertEqual("ABC",hasil)
+
+    def test_insertToDbRekamJejakAllNull(self):
+        mock_npm = "11111111111"
+        mock_kode_matkul = "123"
+        mock_nilai = "A"
+        #kesavedong :()
+        return True
+
+    def test_createMockDataDosen(self):
+        return True
--- api/db/utils.py
+++ api/db/utils.py
@@ -1,21 +1,51 @@
 from api.siak import get_academic_record, get_access_token
 from api.siak.utils import AuthGenerator, Requester
+from api.models import *
+from random import randint
 
-def credentialGenerator(self):
-	token = get_access_token(self, muhammad.faiz52, 15kukusan)
-	return token
-
-def getSiakData(self, npm, username, password):
+def getSiakData(npm, username, password):
 	#kalo buat semuanya paling ITF , yang kita cuma bisa ambil 6 aja
 	res = get_academic_record(npm, username, password)
-	print(res)
 	return res
 	
-def insertToDb(self):
+def parseSiakData(npm, username, password):
+	data = getSiakData(npm, username, password)
+	return "ABC"
+
+def insertToDbRekamJejak(npm, kode_matkul, nilai):
+	if(Mahasiswa.objects.filter(npm=npm).count() < 1):
+		createMahasiswa(npm=npm)
+	if(MataKuliah.objects.filter(kode_matkul=kode_matkul).count() < 1):
+		createMataKuliah(kode_matkul=kode_matkul)
+	rj = RekamJejakNilaiMatakuliah(npm=npm, kode_matkul=kode_matkul, nilai=nilai)
+	rj.save()
+
+def createMahasiswa(npm, nama="nama_def", prodi="Tanpa Prodi", program="Tanpa Jenjang", nip_pa=""):		
+	if(Dosen.objects.filter(nip=nip_pa).count() < 1):
+		if(nip_pa==""):
+			nip_pa="123456789" #default dosen
+		else:
+			createDosen(nip=nip_pa,is_pa=True)
+
+	m = Mahasiswa(npm=npm, nama=nama, study_program=prodi, educational_program=program)
+	m.nip_pa = nip_pa
+	m.save()
+
+def createDosen(nama, nip, is_pa=False):
+	d = Dosen(nama="namaDef", nip=nip, is_pa=is_pa)
+	d.save()
+
+def createMockDataMahasiswa(self):
 	pass
 
-def populateDbWithMock(self):
-	pass
-
-def createMockData(self):
-	pass
+def createMockDataDosen(jumlah):
+	nama = "nama"
+	is_pa = True
+	for i in range(1,jumlah):
+		namaCur=nama+str(i)
+		nip=randint(100000000,999999999)
+		dosen = Dosen(nama=namaCur, nip=str(nip), is_pa=is_pa)
+		is_pa = True
+		if(randint(1,10) > 8):
+			is_pa = False
+		dosen.save()
