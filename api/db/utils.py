1a046cc4221199208ddb6476e7e35549a1f646e2
muhammadfaiz12 <faiz00.muhammad@gmail.com>
2018-03-19 12:00:37 +0700

    committ api/db/utis stub and importing siak dari branch sebelah

---
+++ api/db/utils.py
@@ -0,0 +1,17 @@
+from api.siak import get_academic_record, get_access_token
+from api.siak.utils import AuthGenerator, Requester
+
+def credentialGenerator(self):
+	pass
+
+def getSiakData(self):
+	pass
+
+def insertToDb(self):
+	pass
+
+def populateDbWithMock(self):
+	pass
+
+def createMockData(self):
+	pass
---
+++ api/siak/utils.py
@@ -0,0 +1,55 @@
+import requests
+
+class AuthGenerator:
+    def __init__(self):
+        self.api_mahasiswa = "https://api-dev.cs.ui.ac.id/siakngcs/mahasiswa/"
+        self.api_verify_user = "https://akun.cs.ui.ac.id/oauth/token/verify/"
+        self.api_token = "https://akun.cs.ui.ac.id/oauth/token/"
+        self.client_id = "X3zNkFmepkdA47ASNMDZRX3Z9gqSU1Lwywu5WepG"
+
+    def get_client_id(self):
+        return self.client_id
+
+    def get_access_token(self, username, password):
+        payload = "username={}&password={}&grant_type=password".format(username, password)
+        auth_hash = "Basic WDN6TmtGbWVwa2RBNDdBU05NRFpSWDNaOWdxU1UxTHd5d3U1V2VwRzpCRVFXQW43RD" \
+                    "l6a2k3NEZ0bkNpWVhIRk50Ymg3eXlNWmFuNnlvMU1uaUdSVWNGWnhkQnBobUU5T" \
+                    "UxuVHZiTTEzM1dsUnBwTHJoTXBkYktqTjBxcU9OaHlTNGl2Z0" \
+                    "doczB0OVhlQ3M0Ym1JeUJLMldwbnZYTXE4VU5yTEFEMDNZeA=="
+        headers = {
+            'authorization': auth_hash,
+            'cache-control': "no-cache",
+            'content-type': "application/x-www-form-urlencoded"
+        }
+        response = requests.post(self.api_token, data=payload, headers=headers)
+
+        if response.status_code == 401:
+            raise Exception("Wrong username or password, input: {}, {}".format(username, password,))
+
+        return response.json()["access_token"]
+
+    def verify_user(self, access_token):
+        parameters = {"access_token": access_token, "client_id": self.get_client_id()}
+        response = requests.get(self.api_verify_user, params=parameters)
+        if response.status_code == 403:
+            raise Exception("{} input : {}".format(response.json()['detail'], access_token))
+        return response.json()
+
+    def get_data_user(self, access_token, npm):
+        parameters = {"access_token": access_token, "client_id": self.get_client_id()}
+        response = requests.get(self.api_mahasiswa+npm, params=parameters)
+        if response.status_code == 403:
+            raise Exception("{} input: {}, {}".format(response.json()['detail'], access_token, npm))
+        return response.json()
+
+class Requester:
+
+    @staticmethod
+    def request_academic_data(npm, client_id, token):
+        url = "https://api.cs.ui.ac.id/siakngcs/mahasiswa" \
+              "/{}/riwayat/?access_token={}&client_id={}".format(npm, token, client_id)
+        response = requests.get(url)
+        if response.status_code == 403:
+            err_msg = response.json()['detail']
+            raise Exception("{} input: {}, {}, {}".format(err_msg, npm, client_id, token))
+        return response.json()
