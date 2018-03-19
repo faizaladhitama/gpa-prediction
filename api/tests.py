9e08b0fb54338bb2258ca76ffb0b3e7589ffb21a
muhammadfaiz12 <faiz00.muhammad@gmail.com>
2018-03-19 11:44:47 +0700

    removing unnecasry tests from api/test.py because of already been implemented in api/siak/test.py

--- api/tests.py
+++ api/tests.py
@@ -9,11 +9,3 @@ class URLTest(TestCase):
     def test_logout(self):
         response = self.client.get('/logout', follow=True)
         self.assertEqual(response.status_code, 200)
-
-class APICredentialGenerator(TestCase):
-    def test_get_clientid:
-        assert(get_client_id() == "X3zNkFmepkdA47ASNMDZRX3Z9gqSU1Lwywu5WepG")
-    def test_get_access_token:
-        pass
-    
-
