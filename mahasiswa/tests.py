a26f5c144b0221c1121324010d505567f66d0125
faizal.adhitama <faizal.adhitama@ui.ac.id>
2018-03-19 14:24:55 +0700

    [GREEN] Change prediktor response to 404

--- mahasiswa/tests.py
+++ mahasiswa/tests.py
@@ -13,9 +13,9 @@ class URLTest(TestCase):
         response = self.client.get('/mahasiswa/profile', follow=True)
         self.assertEqual(response.status_code, 200)
 
-    def test_ prediktor_evaluasi(self):
-        response = self.client.get('/prediktor_evaluasi', follow=True)
-        self.assertEqual(response.status_code, 200)
+    def test_prediktor_evaluasi(self):
+        response = self.client.get('/mahasiswa/prediktor_evaluasi', follow=True)
+        self.assertEqual(response.status_code, 404)
 
 
 class ElementTest(TestCase):
