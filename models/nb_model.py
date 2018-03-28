aa75df561a8baa57aff17203db98e3a1045f6c9a
muhammadfaiz12 <faiz00.muhammad@gmail.com>
2018-03-29 01:45:31 +0700

    [RED] fixing nbModel

--- models/nb_model.py
+++ models/nb_model.py
@@ -1,46 +1,53 @@
+import pandas as pd
+import numpy as np
+import pickle
+import os.path
+from sklearn.preprocessing import Imputer
 from sklearn import preprocessing
 from sklearn.cross_validation import train_test_split
-from sklearn.metrics import accuracy_score
 from sklearn.naive_bayes import GaussianNB
-from sklearn.preprocessing import Imputer
-import numpy as np
-import pandas as pdimport picklenm
+from sklearn.metrics import accuracy_score
 
-class Nb_Model:
-	def __init__(name, columns, num_features):
+class NbModel:
+	def __init__(self, name, columns, num_features):
 		self.course_name = str(name)
 		self.columns = columns
 		self.num_features = num_features
 		self.accuracy = 0
 		self.clf = GaussianNB()
+		self.df = None
 
 	def create_model(self):
-		address = "data/"+self.course_name+".csv"
+		pwd = os.path.dirname(__file__)
+		address = pwd+"/data/"+self.course_name+".csv"
 		df = pd.read_csv(address, header=None, delimiter=",", engine='python')
 		df.columns = self.columns
 		df_rev = df
 
 		le = preprocessing.LabelEncoder()
-		cat_list = {}
+		cat_list = []
 		for col in df.columns:
-			cat_list.append(le.fit_transform(df_rev.col))
+			cat_list.append(le.fit_transform(getattr(df_rev, col)))
 
-		info = list(zip(df.columns, cat_list))
+		column_cat_value = list(zip(df.columns, cat_list))
 
-		for col in info(0):
-			df_rev[col] = info(1)
+		for col in column_cat_value:
+			df_rev[col[0]] = col[1]
+
+		self.df = df_rev
 
 		return df_rev
 
 	def train_model(self):
+		df = self.df
 		scaled_features = {}
-		for feature in num_features:
+		for feature in self.num_features:
 			mean, std = df[each].mean(), df[each].std()
 			scaled_features[each] = [mean, std]
 			df.loc[:, each] = (df[each] - mean)/std	
 
 		feature = df.values[:,:len(self.num_features)]
-		target = df.valuse[:,len(self.num_features)] #au saya juga bingung
+		target = df.valuse[:,len(self.num_features)] #au saya juga bingung , cc Gibran MFW aja
 		features_train, features_test, target_train, target_test = train_test_split(features, target, test_size = 0.33, random_state = 10)
 
 		self.clf.fit(features_train, target_train)
