b9a87a615af728dbcecb4dc7a3c146a76193aa04
muhammadfaiz12 <faiz00.muhammad@gmail.com>
2018-03-29 16:53:08 +0700

    [RED] commting test unit and editing django-lint and django-test , somehow test unit doesnot run

--- models/test.py
+++ models/test.py
@@ -1,9 +1,37 @@
+from unittest.mock import patch
+from django.test import TestCase
+import pandas as pd
+import os.path
+from models.nb_model import NbModel
+
 class NbModelTest(TestCase):
+	def setUp(self):
+		kolom = ['nilaiDumA', 'nilaiDumB', 'absDumA', 'absDumB', 'IP', 'status']
+		fitur = ['nilaiDumA', 'nilaiDumB', 'absDumA', 'absDumB', 'IP',]
+		self.model = NbModel("dummy", kolom, fitur)
+	
 	def test_create(self):
-		pass
+		kolom = ['nilaiDumA', 'nilaiDumB', 'absDumA', 'absDumB', 'IP', 'status']
+		df = self.model.create_model()
+		assertEqual(len(df.columns,len(kolom)))
+
 	def test_train(self):
-		pass
+		self.model.train_model()
+		return self.accuracy != None and self.accuracy >= 0
+
 	def test_save(self):
-		pass
+		self.model.save_model()
+		pwd = os.path.dirname(__file__)
+		file_name = pwd+'/savefile/'+self.course_name+'.sav'
+		return os.path.isfile(fn)
+
 	def test_build(Self):
-		pass
+		self.model.build_model()
+		flag1 = self.accuracy != None
+		flag2 = self.df != None
+		return flag1 and flag2
+
+	def tearDown(self):
+		pwd = os.path.dirname(__file__)
+		file_name = pwd+'/savefile/'+self.course_name+'.sav'
+		os.remove(file_name)
