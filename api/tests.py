dd60cc283b8d5a56f9a16532154a3cbb0a685e17
sarah df <sarahdfxo@gmail.com>
2018-03-31 08:37:39 +0700

    remove prediktor test

--- api/tests.py
+++ api/tests.py
@@ -95,10 +95,6 @@ class URLTest(TestCase):
         response = self.client.get('', follow=True)
         self.assertEqual(response.status_code, 200)
 
-    def test_prediktor_matkul_url_is_exist(self):
-		response = self.client.get('/prediktor-matkul', follow=True)
-		self.assertEqual(response.status_code, 200)
-
 
 class UserTest(TestCase):
     def test_auth_login_positive(self):
