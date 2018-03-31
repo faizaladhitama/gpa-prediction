7b4815a21f8a4288e885456d1dd42703fa5abdef
muhammadfaiz12 <faiz00.muhammad@gmail.com>
2018-03-31 22:53:35 +0700

    [RED]fixing nilai preprocessing in api.models . get_prediction

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -1,6 +1,9 @@
+import numpy as np
 from api.models.nb_model import NbModel
 
+
 def get_prediction(mata_kuliah, nilai):
+	nilai = np.asarray(nilai.reshape(1,-1))
 	prediksi = NbModel(mata_kuliah,num_features=nilai)
 	prediksi.load_model()
 	hasil = prediksi.predict(nilai)
