b71cd652ee7b74955345a8dbed52b1b9f13bcb70
sarah df <sarahdfxo@gmail.com>
2018-03-31 08:28:06 +0700

    add prediktor matkul test

--- api/tests.py
+++ api/tests.py
@@ -7,7 +7,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.support import expected_conditions as EC
-
+		
 
 class SeleniumTestCase(LiveServerTestCase):
     @classmethod
@@ -94,6 +94,10 @@ class URLTest(TestCase):
     def test_landing(self):
         response = self.client.get('', follow=True)
         self.assertEqual(response.status_code, 200)
+
+    def test_prediktor_matkul_url_is_exist(self):
+		response = self.client.get('/prediktor-matkul', follow=True)
+		self.assertEqual(response.status_code, 200)
 
 
 class UserTest(TestCase):
