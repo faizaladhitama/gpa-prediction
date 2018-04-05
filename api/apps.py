461739005a2e3f7cd233e9beb7d7208c044d001b
gibranmfw <gibranmfw@gmail.com>
2018-04-05 09:10:43 +0700

    [RED] uncomment api.apps and mahasiswa.utils

--- api/apps.py
+++ api/apps.py
@@ -1,16 +1,16 @@
-# from django.apps import AppConfig
+from django.apps import AppConfig
 
 
-# class ApiConfig(AppConfig):
-#     name = 'api'
+class ApiConfig(AppConfig):
+    name = 'api'
 
-# class prediksiEvaluasiAkademik():
-#     def give_verdict(npm, sks_lulus, sks_diambil, ip):
-#         status = "Tidak Lolos"
-#         sks_kemungkinan = sks_lulus + sks_diambil
-#         if(sks_lulus >= sks_minimal):
-#             status = "Lolos"
-#         elif(sks_lulus < sks_minimal and sks_kemungkinan >= sks_minimal):
-#             status = "Hati Hati"
-#         #ip buat apa ya? harus check aturan akademis dulu
-#         return status
+class prediksiEvaluasiAkademik():
+    def give_verdict(npm, sks_lulus, sks_diambil, ip):
+        status = "Tidak Lolos"
+        sks_kemungkinan = sks_lulus + sks_diambil
+        if(sks_lulus >= sks_minimal):
+            status = "Lolos"
+        elif(sks_lulus < sks_minimal and sks_kemungkinan >= sks_minimal):
+            status = "Hati Hati"
+        #ip buat apa ya? harus check aturan akademis dulu
+        return status
