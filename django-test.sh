9ef0cd134ca2d3e588d823e88e43132dd095f4cd
gibranmfw <gibranmfw@gmail.com>
2018-04-05 09:36:01 +0700

    [REFACTOR] fix django-test.sh

--- django-test.sh
+++ django-test.sh
@@ -1,7 +1,3 @@
 coverage run --include='api/*','mahasiswa/*','dosen/*','pa/*','sekre/*' manage.py test
 coverage report
-<<<<<<< HEAD
 coverage html
-=======
-coverage html
->>>>>>> 10b4c1826d2f64a1b254fe830a3d10f15fd60b87
