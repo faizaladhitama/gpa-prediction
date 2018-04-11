50a59d46f2e8726656281558921f79b1fb06f8dd
muhammadfaiz12 <faiz00.muhammad@gmail.com>
2018-04-11 15:20:43 +0700

    [RED] adding some implementaion for test

--- api/db/utils.py
+++ api/db/utils.py
@@ -1,6 +1,6 @@
 from random import randint
 from api.siak import get_academic_record
-from api.models import Dosen, Mahasiswa, RekamJejakNilaiMataKuliah, MataKuliah
+from api.models import Dosen, Mahasiswa, RekamJejakNilaiMataKuliah, MataKuliah, MahasiswaSIAK
 
 def get_siak_data(npm, username, password):
     #kalo buat semuanya paling ITF , yang kita cuma bisa ambil 6 aja
@@ -28,6 +28,18 @@ def create_mahasiswa(npm, nama="nama_def", prodi="Tanpa Prodi", program="Tanpa J
         create_dosen(nip=nip_pa, is_pa=True)
 
     ma_new = Mahasiswa(npm=npm, nama=nama, study_program=prodi, educational_program=program)
+    ma_new.nip_pa = Dosen.objects.get(nip=nip_pa)
+    ma_new.save()
+
+def create_mahasiswa_siak(npm, nama="nama_def", prodi="Tanpa Prodi", program="Tanpa Jenjang", nip_pa=""):
+    if Dosen.objects.filter(nip=nip_pa).count() < 1:
+        if nip_pa == "":
+            nip_pa = "1234567890" #default dosen
+        #else:
+        create_dosen(nip=nip_pa, is_pa=True)
+
+    ma_new = MahasiswaSIAK(npm=npm, nama=nama, study_program=prodi, educational_program=program)
+    ma_new.status_evaluasi = False
     ma_new.nip_pa = Dosen.objects.get(nip=nip_pa)
     ma_new.save()
 
--- api/tests.py
+++ api/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
-from api.apps import give_verdict, save_status
+from api.apps import ApiConfig, give_verdict, save_status
 from api.models import MahasiswaSIAK
-from api.db.utils import create_dosen, create_mahasiswa
+from api.db.utils import create_dosen, create_mahasiswa_siak
 # from django.conf import settings
 # from django.test import Client
 # from django.test import LiveServerTestCase
@@ -134,6 +134,9 @@ class ExternalAPITest(TestCase):
         self.assertEqual(response.status_code, 200)
 
 class EvaluasiTest(TestCase):
+    def test_api_config(self):
+        # = ApiConfig('api', 'api.apps')
+        self.assertEqual(True, True)
     def test_rumus_lolos(self):
         hasil = give_verdict(48, 48, 19, 3.3)
         self.assertEqual(hasil, "Lolos")
@@ -148,18 +151,17 @@ class EvaluasiTest(TestCase):
 
     def test_save_status(self):
         npm = '1506111222'
-        create_mahasiswa(npm)
+        create_mahasiswa_siak(npm)
         save_status(npm, True)
         flag = MahasiswaSIAK.objects.get(npm=npm).status_evaluasi
         self.assertEqual(flag, True)
 
     def test_save_status_false(self):
         npm = '1506333444'
-        create_mahasiswa(npm)
+        create_mahasiswa_siak(npm)
         save_status(npm, False)
         flag = MahasiswaSIAK.objects.get(npm=npm).status_evaluasi
         self.assertEqual(flag, False)
-
 
     def test_save_status_notFound(self):
         hasil = save_status('6969696969', False)
