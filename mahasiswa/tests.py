7c86e61ae1d8c5f383b6d0e3f79b2c44eee567af
gibranmfw <gibranmfw@gmail.com>
2018-04-11 14:49:48 +0700

    [RED] fixed some test error

--- mahasiswa/tests.py
+++ mahasiswa/tests.py
@@ -68,35 +68,35 @@ class ContextTest(TestCase):
         context = get_context_mahasiswa(request, get_term(datetime.now()))
         self.assertEqual(context, "'user_login'")
 
-
 class EvaluationTest(TestCase):
+    def setUp(self):
+        self.true_source = "Keputusan Rektor Universitas Indonesia\
+            Nomor: 478/SK/R/UI/2004 tentang Evaluasi\
+            Keberhasilan Studi Mahasiswa Universitas\
+            Indonesia Pasal 11"
+
     def test_detail_valid_all(self):
         detail_message = get_evaluation_detail_message("S1", 2)
         source = detail_message['source']
         detail = detail_message['detail']
-        self.assertEqual(None, source)
-        self.assertEqual(None, detail)
+        self.assertEqual(self.true_source, source)
+        self.assertEqual('Apabila pada evaluasi 2 (dua) semester pertama \
+                 tidak memperoleh indeks prestasi minimal 2,0 \
+                 (dua koma nol) dari sekurang-kurangnya 24 \
+                 (dua puluh empat) SKS terbaik', detail)
+
+    def test_detail_valid_degree_only(self):
+        detail_message = get_evaluation_detail_message("S1", -1)
+        self.assertEqual("Wrong degree and semester", detail_message)
 
     def test_detail_valid_semester_only(self):
-        detail_message = get_evaluation_detail_message("S1", -1)
-        source = detail_message['source']
-        detail = detail_message['detail']
-        self.assertEqual(None, source)
-        self.assertEqual(None, detail)
-
-    def test_detail_valid_ip_only(self):
         detail_message = get_evaluation_detail_message("S-teh", 2)
-        source = detail_message['source']
-        detail = detail_message['detail']
-        self.assertEqual(None, source)
-        self.assertEqual(None, detail)
+        self.assertEqual("Wrong degree and semester", detail_message)
 
     def test_detail_invalid_all(self):
         detail_message = get_evaluation_detail_message("S-teh", -1)
-        source = detail_message['source']
-        detail = detail_message['detail']
-        self.assertEqual(None, source)
-        self.assertEqual(None, detail)
+        self.assertEqual("Wrong degree and semester", detail_message)
+
 
 
 class SemesterTest(TestCase):
