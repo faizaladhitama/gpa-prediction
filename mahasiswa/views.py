0bc1277190408a6f7d862b9b12ec74365dfc97b9
sarah df <sarahdfxo@gmail.com>
2018-04-13 17:06:04 +0700

    [RED] implement get evaluation status

--- mahasiswa/views.py
+++ mahasiswa/views.py
@@ -12,10 +12,20 @@ from api.siak import get_access_token, get_data_user, get_all_sks_term
 def index(request):
     now = datetime.now()
     term_str = get_term(now)
+    token = get_access_token(username, password)
+   
     try:
         context_mahasiswa = get_context_mahasiswa(request, term_str)
         context = get_index_mahasiswa_context(request, context_mahasiswa,
                                               term_str)
+        npm = context_mahasiswa['kode_identitas']
+        username = context_mahasiswa['user']
+        password = 'aa'
+        sks_seharusnya = 12*term
+        sks_kurang = sks_seharusnya - get_all_sks_term(token, npm)
+        context.update({'sks_kurang' : sks_kurang})
+        status = request_evaluation_status(npm, username, password, term)
+        context.update({'status' : status})
         return render(request, 'mahasiswa/index.tpl', context)
     except TypeError:
         return render(request, 'landing_page.tpl', {})
@@ -29,26 +39,3 @@ def profile(request):
 def rekomendasi(request):
     context = {'name': 'mahasiswa'}
     return render(request, 'mahasiswa/rekomendasi.tpl', context)
-
-
-def get_evaluation_status():
-    now = datetime.now()
-    term_str = get_term(now)
-    token = get_access_token(username, password)
-    context_mahasiswa = get_context_mahasiswa(request, term_str)
-    npm = context_mahasiswa['kode_identitas']
-    username = context_mahasiswa['user']
-    pwd = 'aa'
-    status = request_evaluation_status(npm, username, password, term)
-    if status == 'lolos':
-        status_button = '<button type="button" class="btn btn-success btn-lg btn3d">lolos</button>'
-        return HttpResponse(status_button, sks_kurang)
-    elif status == 'hati-hati':
-        status_button = '<button type="button" class="btn btn-warning btn-lg btn3d">hati-hati</button>'
-        sks_seharusnya = 12*term
-        sks_kurang = sks_seharusnya - get_all_sks_term(token, npm)
-        return HttpResponse(status_button, sks_kurang)
-    else:
-        status_button = '<button type="button" class="btn btn-success btn-lg btn3d">tidak <br>lolos</button>'
-        sks_kurang = sks_seharusnya - get_all_sks_term(token, npm)
-        return HttpResponse(status_button, sks_kurang)
