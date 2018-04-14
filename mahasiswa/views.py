c8cc3f9522b3d31a9a2e842b8c5c8ec6ae0dec98
sarah df <sarahdfxo@gmail.com>
2018-04-14 13:14:36 +0700

    [RED] modify prediktor evaluasi

--- mahasiswa/views.py
+++ mahasiswa/views.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 from django.shortcuts import render
 
-from mahasiswa.utils import request_evaluation_status, get_term, get_context_mahasiswa, \
+from mahasiswa.utils import request_evaluation_status, get_semester, get_term, get_context_mahasiswa, \
     get_index_mahasiswa_context
 
 from api.siak import get_access_token, get_data_user, get_all_sks_term
@@ -26,6 +26,8 @@ def index(request):
         context.update({'sks_kurang' : sks_kurang})
         status = request_evaluation_status(npm, username, password, term)
         context.update({'status' : status})
+        semester = get_semester(npm, term_str)
+        context.update({'semester' : semester})
         return render(request, 'mahasiswa/index.tpl', context)
     except TypeError:
         return render(request, 'landing_page.tpl', {})
