99985376c11a83942ba593be2ef7c96e88fe4c50
faizal.adhitama <faizal.adhitama@ui.ac.id>
2018-04-25 15:29:12 +0700

    Linter adjusment

--- mahasiswa/views.py
+++ mahasiswa/views.py
@@ -2,10 +2,9 @@ from datetime import datetime
 
 from django.shortcuts import render
 
+from api.siak import get_sks, get_all_sks_term
 from mahasiswa.utils import get_term, get_context_mahasiswa, \
     get_index_mahasiswa_context, get_semester, request_evaluation_status
-
-from api.siak import get_sks, get_all_sks_term
 
 
 # Create your views here.
@@ -29,10 +28,10 @@ def index(request):
             print(err)
         if username != "admin":
             sks_kurang = sks_seharusnya - all_sks
-            context.update({'sks_kurang' : sks_kurang})
+            context.update({'sks_kurang': sks_kurang})
             status = request_evaluation_status(npm, request.session['access_token'], semester)
-            context.update({'status' : status})
-            context.update({'semester' : semester})
+            context.update({'status': status})
+            context.update({'semester': semester})
             all_sks_term, err = get_all_sks_term(request.session['access_token'], npm)
             context.update({'sks_term': all_sks_term})
         return render(request, 'mahasiswa/index.tpl', context)
