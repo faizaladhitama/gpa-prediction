613041f3da41a144d4959b3214bdc866eb361d96
faiz97 <maulafaiz97@gmail.com>
2018-04-26 11:15:09 +0700

    [RED] copying necessary files from branch prediksi_lolos_evaluasi_akademik

--- api/siak/__init__.py
+++ api/siak/__init__.py
@@ -1,14 +1,44 @@
+import datetime
+import os
+
 import requests
+
 from api.siak.utils import AuthGenerator, Requester
+
+
+def cek_huruf_lulus(huruf):
+    if huruf == "C-":
+        return False
+    elif "A" in huruf or "B" in huruf or "C" in huruf:
+        return True
+    else:
+        return False
+
+
+def huruf_to_angka(huruf):
+    bobot = {
+        'A': 4.00,
+        'A-': 3.70,
+        'B+': 3.30,
+        'B': 3.00,
+        'B-': 2.70,
+        'C+': 2.30,
+        'C': 2.00,
+        'C-': 1.70,
+        'D': 1.00,
+        'E': 0.00,
+        'N': 0.00
+    }
+    return bobot[huruf]
+
 
 def get_academic_record(npm, username, password):
     try:
         generator = AuthGenerator()
+        client_id = os.environ['CLIENT_ID']
+        token = generator.get_access_token(username, password, os.environ['AUTH_HASH'])
 
-        client_id = generator.get_client_id()
-        token = generator.get_access_token(username, password)
-
-        if generator.verify_user(token)['username'] == username:
+        if generator.verify_user(token, client_id)['username'] == username:
             res = Requester.request_academic_data(npm, client_id, token)
             return res
         else:
@@ -18,31 +48,158 @@ def get_academic_record(npm, username, password):
     except requests.ConnectionError as exception:
         return str(exception)
 
+
 def get_access_token(username, password):
     try:
         generator = AuthGenerator()
-        return generator.get_access_token(username, password)
+        return generator.get_access_token(username, password, os.environ['AUTH_HASH'])
     except ValueError as exception:
         return str(exception)
     except requests.ConnectionError as exception:
         return str(exception)
+
 
 def verify_user(access_token):
     try:
         generator = AuthGenerator()
         if access_token == "12345678910ABCDEFGHIJKLMNOPQRSTUVWXYZ":
             return {"identity_number": 'admin', "role": 'mahasiswa'}
-        return generator.verify_user(access_token)
+        return generator.verify_user(access_token, os.environ['CLIENT_ID'])
     except ValueError as exception:
         return str(exception)
     except requests.ConnectionError as exception:
         return str(exception)
 
+
 def get_data_user(access_token, npm):
     try:
         generator = AuthGenerator()
-        return generator.get_data_user(access_token, npm)
+        return generator.get_data_user(access_token, npm, os.environ['CLIENT_ID']), None
     except ValueError as exception:
-        return str(exception)
+        return None, str(exception)
     except requests.ConnectionError as exception:
-        return str(exception)
+        return None, str(exception)
+
+
+def get_sks(access_token, npm):
+    try:
+        data = Requester.request_mahasiswa_data(npm, os.environ['CLIENT_ID'], access_token)
+        angkatan = data['program'][0]['angkatan']
+
+        now = datetime.datetime.now()
+
+        tot_sks = 0
+
+        for year in range(int(angkatan), now.year + 1):
+            for term in range(1, 4):
+                res = Requester.request_sks(npm, term, year, os.environ['CLIENT_ID'], access_token)
+                for course in res:
+                    if course['kelas'] != None:
+                        tot_sks = tot_sks + course['kelas']['nm_mk_cl']['jml_sks']
+
+        return tot_sks, None
+    except ValueError as exception:
+        return None, str(exception)
+    except requests.ConnectionError as exception:
+        return None, str(exception)
+
+
+def get_all_sks_term(access_token, npm):
+    try:
+        data = Requester.request_mahasiswa_data(npm, os.environ['CLIENT_ID'], access_token)
+        angkatan = data['program'][0]['angkatan']
+
+        now = datetime.datetime.now()
+
+        sks_map = {}
+
+        for year in range(int(angkatan), now.year + 1):
+            sks_terms = []
+            for term in range(1, 4):
+                tot_sks = 0
+                res = Requester.request_sks(npm, term, year, os.environ['CLIENT_ID'], access_token)
+                for course in res:
+                    if course['kelas'] != None and cek_huruf_lulus(course['nilai']):
+                        tot_sks = tot_sks + course['kelas']['nm_mk_cl']['jml_sks']
+
+                sks_terms.append(tot_sks)
+            sks_map[year] = sks_terms
+        return sks_map, None
+    except ValueError as exception:
+        return {}, str(exception)
+    except requests.ConnectionError as exception:
+        return {}, str(exception)
+
+
+def get_sks_term(access_token, npm, year, term):
+    try:
+        res = Requester.request_sks(npm, term, year, os.environ['CLIENT_ID'], access_token)
+        tot_sks = 0
+
+        for course in res:
+            if course['kelas'] != None and cek_huruf_lulus(course['nilai']):
+                tot_sks = tot_sks + course['kelas']['nm_mk_cl']['jml_sks']
+        return tot_sks, None
+    except ValueError as exception:
+        return 0, str(exception)
+    except requests.ConnectionError as exception:
+        return 0, str(exception)
+
+
+def get_jenjang(access_token, npm):
+    res, err = get_data_user(access_token, npm)
+    if err is None:
+        return res['program'][0]['nm_prg'], None
+    else:
+        return None, err
+
+
+def get_ip_term(access_token, npm, year, term):
+    try:
+        res = Requester.request_sks(npm, term, year, os.environ['CLIENT_ID'], access_token)
+        tot_sks = 0
+        mutu = 0.00
+
+        for course in res:
+            if course['kelas'] != None:
+                tot_sks = tot_sks + course['kelas']['nm_mk_cl']['jml_sks']
+                mutu += course['kelas']['nm_mk_cl']['jml_sks'] * huruf_to_angka(course['nilai'])
+        ip_mahasiswa = round(mutu / tot_sks * 100)/100.00
+        return ip_mahasiswa, None
+    except ValueError as exception:
+        return 0, str(exception)
+    except requests.ConnectionError as exception:
+        return 0, str(exception)
+
+
+def get_all_ip_term(access_token, npm):
+    try:
+        data = Requester.request_mahasiswa_data(npm, os.environ['CLIENT_ID'], access_token)
+        angkatan = data['program'][0]['angkatan']
+
+        now = datetime.datetime.now()
+
+        ip_map = {}
+
+        for year in range(int(angkatan), now.year + 1):
+            ip_terms = []
+            for term in range(1, 4):
+                tot_sks = 0
+                mutu = 0.00
+                res = Requester.request_sks(npm, term, year, os.environ['CLIENT_ID'], access_token)
+                for course in res:
+                    if course['kelas'] != None:
+                        tot_sks = tot_sks + course['kelas']['nm_mk_cl']['jml_sks']
+                        mutu += course['kelas']['nm_mk_cl']['jml_sks'] * \
+                                huruf_to_angka(course['nilai'])
+                if tot_sks != 0:
+                    ip_mahasiswa = round(mutu / tot_sks * 100) / 100.00
+                else:
+                    ip_mahasiswa = 0.00
+                ip_terms.append(ip_mahasiswa)
+            ip_map[year] = ip_terms
+        return ip_map, None
+    except ValueError as exception:
+        return {}, str(exception)
+    except requests.ConnectionError as exception:
+        return {}, str(exception)
--- api/siak/utils.py
+++ api/siak/utils.py
@@ -1,22 +1,45 @@
 import requests
 
+class Requester:
+    @staticmethod
+    def request_academic_data(npm, client_id, token):
+        url = "https://api.cs.ui.ac.id/siakngcs/mahasiswa" \
+              "/{}/riwayat/?access_token={}&client_id={}".format(npm, token, client_id)
+        response = requests.get(url)
+        if response.status_code == 403:
+            err_msg = response.json()['detail']
+            raise ValueError(err_msg)
+        return response.json()
+
+    @staticmethod
+    def request_mahasiswa_data(npm, client_id, token):
+        url = "https://api-dev.cs.ui.ac.id/siakngcs/mahasiswa/"
+        url = "{}{}/?client_id={}&access_token={}".format(url, npm, client_id, token)
+        response = requests.get(url)
+        if response.status_code == 403:
+            err_msg = response.json()['detail']
+            raise ValueError(err_msg)
+        return response.json()
+
+    @staticmethod
+    def request_sks(npm, term, year, client_id, token):
+        url = "https://api-dev.cs.ui.ac.id/siakngcs/mahasiswa/"
+        url = "{}{}/riwayat/{}/{}/?" \
+              "client_id={}&access_token={}".format(url, npm, year, term, client_id, token)
+        response = requests.get(url)
+        if response.status_code == 403:
+            err_msg = response.json()['detail']
+            raise ValueError(err_msg)
+        return response.json()
 
 class AuthGenerator:
     def __init__(self):
         self.api_mahasiswa = "https://api-dev.cs.ui.ac.id/siakngcs/mahasiswa/"
         self.api_verify_user = "https://akun.cs.ui.ac.id/oauth/token/verify/"
         self.api_token = "https://akun.cs.ui.ac.id/oauth/token/"
-        self.client_id = "X3zNkFmepkdA47ASNMDZRX3Z9gqSU1Lwywu5WepG"
 
-    def get_client_id(self):
-        return self.client_id
-
-    def get_access_token(self, username, password):
+    def get_access_token(self, username, password, auth_hash):
         payload = "username={}&password={}&grant_type=password".format(username, password)
-        auth_hash = "Basic WDN6TmtGbWVwa2RBNDdBU05NRFpSWDNaOWdxU1UxTHd5d3U1V2VwRzpCRVFXQW43RD" \
-                    "l6a2k3NEZ0bkNpWVhIRk50Ymg3eXlNWmFuNnlvMU1uaUdSVWNGWnhkQnBobUU5T" \
-                    "UxuVHZiTTEzM1dsUnBwTHJoTXBkYktqTjBxcU9OaHlTNGl2Z0" \
-                    "doczB0OVhlQ3M0Ym1JeUJLMldwbnZYTXE4VU5yTEFEMDNZeA=="
         headers = {
             'authorization': auth_hash,
             'cache-control': "no-cache",
@@ -26,32 +49,20 @@ class AuthGenerator:
             return "12345678910ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         response = requests.post(self.api_token, data=payload, headers=headers)
         if response.status_code == 401:
-            raise ValueError("Wrong username or password, input: {}, {}".format(username, password))
+            raise ValueError("Wrong username or password")
 
         return response.json()["access_token"]
 
-    def verify_user(self, access_token):
-        parameters = {"access_token": access_token, "client_id": self.get_client_id()}
+    def verify_user(self, access_token, client_id):
+        parameters = {"access_token": access_token, "client_id": client_id}
         response = requests.get(self.api_verify_user, params=parameters)
         if response.status_code == 403:
-            raise ValueError("{} input : {}".format(response.json()['detail'], access_token))
+            raise ValueError("Token not detected")
         return response.json()
 
-    def get_data_user(self, access_token, npm):
-        parameters = {"access_token": access_token, "client_id": self.get_client_id()}
+    def get_data_user(self, access_token, npm, client_id):
+        parameters = {"access_token": access_token, "client_id": client_id}
         response = requests.get(self.api_mahasiswa + npm, params=parameters)
         if response.status_code == 403:
-            raise ValueError("{} input: {},{}".format(response.json()['detail'], access_token, npm))
-        return response.json()
-
-
-class Requester:
-    @staticmethod
-    def request_academic_data(npm, client_id, token):
-        url = "https://api.cs.ui.ac.id/siakngcs/mahasiswa" \
-              "/{}/riwayat/?access_token={}&client_id={}".format(npm, token, client_id)
-        response = requests.get(url)
-        if response.status_code == 403:
-            err_msg = response.json()['detail']
-            raise ValueError("{} input: {}, {}, {}".format(err_msg, npm, client_id, token))
+            raise ValueError("Can't find user with npm :{}".format(npm))
         return response.json()
