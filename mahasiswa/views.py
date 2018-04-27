6b01964e3a71e043a1524d09566d476cd188b90b
sarah df <sarahdfxo@gmail.com>
2018-04-27 11:49:55 +0700

    add total sks diperoleh

--- mahasiswa/views.py
+++ mahasiswa/views.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 from django.shortcuts import render
 
-from api.siak import get_sks
+from api.siak import get_sks, get_sks_term
 from mahasiswa.utils import get_term, get_context_mahasiswa, \
     get_index_mahasiswa_context, get_semester, request_evaluation_status
 
@@ -14,7 +14,7 @@ def index(request):
     term_str = get_term(now)
     try:
         context_mahasiswa = get_context_mahasiswa(request, term_str)
-        context = get_index_mahasiswa_context(request, context_mahasiswa)
+        context = get_index_mahasiswa_context(request, context_mahasiswa) 
         npm = context_mahasiswa['id']
         username = context_mahasiswa['user']
         term = int(term_str[-1:])
@@ -26,6 +26,7 @@ def index(request):
         context.update({'sks_seharusnya': sks_seharusnya})
         all_sks, err = get_sks(request.session['access_token'], npm)
         context.update({'all_sks' : all_sks})
+        sks_term = get_sks_term(npm, term)
         if err is not None and username != "admin":
             print(err)
         if username != "admin":
