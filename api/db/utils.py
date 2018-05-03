988a8398c0fed2cd272680904a40cb5ac8b1e3e8
faizal.adhitama <faizal.adhitama@ui.ac.id>
2018-05-04 02:31:25 +0700

    Add caching method

--- api/db/utils.py
+++ api/db/utils.py
@@ -1,14 +1,20 @@
 from random import randint
-from api.siak import get_academic_record
+
+from django.core.cache import cache
+
 from api.models import Dosen, Mahasiswa, RekamJejakNilaiMataKuliah, MataKuliah, MahasiswaSIAK
+from api.siak import get_academic_record
+
 
 def get_siak_data(npm, username, password):
-    #kalo buat semuanya paling ITF , yang kita cuma bisa ambil 6 aja
+    # kalo buat semuanya paling ITF , yang kita cuma bisa ambil 6 aja
     return get_academic_record(npm, username, password)
+
 
 def parse_siak_data(npm, username, password):
     data = get_siak_data(npm, username, password)
     return data
+
 
 def insert_to_db_rekam_jejak(npm, kode_matkul, nilai, term=0):
     if Mahasiswa.objects.filter(npm=npm).count() < 1:
@@ -20,22 +26,24 @@ def insert_to_db_rekam_jejak(npm, kode_matkul, nilai, term=0):
     rj_new = RekamJejakNilaiMataKuliah(npm=npm, kode_matkul=kode_matkul, nilai=nilai, term=term)
     rj_new.save()
 
+
 def create_mahasiswa(npm, nama="nama_def", prodi="Tanpa Prodi", program="Tanpa Jenjang", nip_pa=""):
     if Dosen.objects.filter(nip=nip_pa).count() < 1:
         if nip_pa == "":
-            nip_pa = "1123456789" #default dosen
-        #else:
+            nip_pa = "1123456789"  # default dosen
+        # else:
         create_dosen(nip=nip_pa, is_pa=True)
 
     ma_new = Mahasiswa(npm=npm, nama=nama, study_program=prodi, educational_program=program)
     ma_new.nip_pa = Dosen.objects.get(nip=nip_pa)
     ma_new.save()
 
+
 def create_mahasiswa_siak(npm, nama="nama_def", prodi="no Prodi", program="no Jenjang", nip_pa=""):
     if Dosen.objects.filter(nip=nip_pa).count() < 1:
         if nip_pa == "":
-            nip_pa = "1234567890" #default dosen
-        #else:
+            nip_pa = "1234567890"  # default dosen
+        # else:
         create_dosen(nip=nip_pa, is_pa=True)
 
     ma_new = MahasiswaSIAK(npm=npm, nama=nama, study_program=prodi, educational_program=program)
@@ -43,24 +51,59 @@ def create_mahasiswa_siak(npm, nama="nama_def", prodi="no Prodi", program="no Je
     ma_new.nip_pa = Dosen.objects.get(nip=nip_pa)
     ma_new.save()
 
+
 def create_dosen(nip, nama="namadef", is_pa=False):
     do_new = Dosen(nama=nama, nip=nip, is_pa=is_pa)
     do_new.save()
+
 
 def create_matakuliah(kode_matkul, nip=0, nama="namaMatkul", prodi="semua", tkt_krjsama=1, sks=3):
     matkul = MataKuliah(kode_matkul=kode_matkul, nip_pengajar=nip, nama_matkul=nama, prodi=prodi,
                         tingkatKerjasama=tkt_krjsama, sks=sks)
     matkul.save()
 
+
 def create_mock_data_mahasiswa():
     return True
+
 
 def create_mock_data_dosen(jumlah):
     nama = "nama"
     is_pa = True
     for i in range(1, jumlah):
-        nama_cur = nama+str(i)
+        nama_cur = nama + str(i)
         nip = randint(100000000, 999999999)
         dosen = Dosen(nama=nama_cur, nip=str(nip), is_pa=is_pa)
         is_pa = True
         dosen.save()
+
+
+def caching(name, func, args):
+    try:
+        if cache.get(name) is not None:
+            if isinstance(args, tuple):
+                temp = func(*args)
+                if isinstance(temp, dict):
+                    raise TypeError
+                ret, err = temp
+            else:
+                res = func(args)
+                if isinstance(res, str):
+                    raise TypeError
+                ret, err = res
+            cache.set(name, (ret, err))
+        else:
+            ret, err = cache.get(name)
+            print("use cache " + name)
+        return ret, err
+    except (TypeError, ValueError):
+        if cache.get(name) is not None:
+            if isinstance(args, tuple):
+                ret = func(*args)
+            else:
+                ret = func(args)
+            cache.set(name, ret)
+        else:
+            ret = cache.get(name)
+            print("use cache " + name)
+        return ret
