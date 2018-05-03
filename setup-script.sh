6e6e12e4f0b3ac7b023456d0350f1d2369171c51
faizal.adhitama <faizal.adhitama@ui.ac.id>
2018-05-04 01:41:18 +0700

    Setup memcached

--- setup-script.sh
+++ setup-script.sh
@@ -4,10 +4,11 @@ then
     wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
     echo "deb http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list
 fi
+apt-get update -qq && apt-get install -y -qq unzip
+apt-get install -y libmemcached-dev
 pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
-apt-get update -qq && apt-get install -y -qq unzip
 if [[ $1 == "faiz97" || $1 == "sarahdfxo" || $1 == "tinna.fauziah" ]]
 then
     apt-get install -y google-chrome-stable
