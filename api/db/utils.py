3612b9a51e6587d07b381627f637e5e93bc23fee
muhammadfaiz12 <faiz00.muhammad@gmail.com>
2018-05-17 21:20:02 +0700

    changing paramater needed to match db design

--- api/db/utils.py
+++ api/db/utils.py
@@ -30,14 +30,14 @@ def create_mahasiswa(npm, nama="nama_def", prodi="Tanpa Prodi", program="Tanpa J
     ma_new.save()
 
 
-def create_mahasiswa_siak(npm, nama="nama_def", prodi="no Prodi", program="no Jenjang", nip_pa=""):
+def create_mahasiswa_siak(npm, nama="nama_def", prodi="no Prodi", nip_pa=""):
     if Dosen.objects.filter(nip=nip_pa).count() < 1:
         if nip_pa == "":
             nip_pa = "1234567890"  # default dosen
         # else:
         create_dosen(nip=nip_pa, is_pa=True)
 
-    ma_new = MahasiswaSIAK(npm=npm, nama=nama, study_program=prodi, educational_program=program)
+    ma_new = MahasiswaSIAK(npm=npm, nama=nama, study_program=prodi)
     ma_new.status_evaluasi = False
     ma_new.nip_pa = Dosen.objects.get(nip=nip_pa)
     ma_new.save()
