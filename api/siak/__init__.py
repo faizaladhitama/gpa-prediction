deb742c4440f74d92ccdc3553142a286e59a5477
faiz97 <maulafaiz97@gmail.com>
2018-05-26 11:16:33 +0700

    [GREEN] changing return value to dict

--- api/siak/__init__.py
+++ api/siak/__init__.py
@@ -329,7 +329,8 @@ def get_nilai_prasyarat(access_token, npm, nama_matkul):
                 for course in res:
                     if ((course['kelas'] != None) and\
                      (course['kelas']['nm_mk_cl']['nm_mk'] in prasyarat)):
-                        nilai_prasyarat.append(huruf_to_angka(course['nilai']))
+                        prasyarat = course['kelas']['nm_mk_cl']['nm_mk']
+                        nilai_prasyarat[prasyarat] = course['nilai']
         return nilai_prasyarat, None
     except ValueError as exception:
         return {}, str(exception)
