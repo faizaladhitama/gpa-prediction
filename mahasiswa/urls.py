d9843b58865f12a08ffaa9d615af730f96f00eaf
sarah df <sarahdfxo@gmail.com>
2018-05-29 14:52:32 +0700

    [GREEN] implement routing for search matkul

--- mahasiswa/urls.py
+++ mahasiswa/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     url(r'^peraturan-akademik$', views.peraturan_akademik, name='peraturan_akademik'),
     url(r'^riwayat-ip$', views.riwayat_ip, name='riwayat_ip'),
     url(r'^riwayat-sks$', views.riwayat_sks, name='riwayat_sks'),
-    url(r'^prediktor-matkul$', views.prediktor_matkul, name='prediktor_matkul')
+    url(r'^prediktor-matkul$', views.prediktor_matkul, name='prediktor_matkul'),
+    url(r'^search-matkul$', views.search_matkul, name='search_matkul')
 ]
